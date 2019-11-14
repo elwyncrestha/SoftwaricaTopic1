8b9b368c388240a13c2ff088e5f1fd9413ce9745
Elvin Shrestha <elwyncrestha@gmail.com>
2019-11-14 08:29:18 +0545

    Calculator Layout

---
+++ app/src/main/java/com/softwarica/topic1/activity/GridLayoutActivity.java
@@ -0,0 +1,16 @@
+package com.softwarica.topic1.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.softwarica.topic1.R;
+
+public class GridLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_layout);
+    }
+}
